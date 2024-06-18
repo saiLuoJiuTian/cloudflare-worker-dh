e704b5517c8bae22c35ccd79cb825299c08b006d
玖天 <132802737+saiLuoJiuTian@users.noreply.github.com>
2024-06-18 16:03:19 +0800

    Add files via upload

---
+++ resources/js/config.js
@@ -0,0 +1,237 @@
+var bigg = [
+    {
+        name: "搜索引擎",
+        icon: "icon-sousuo_2",
+        bigFly: [
+            {
+                name: "百度",
+                icon: "icon-icon_baidulogo",
+                url: "https://www.baidu.com/"
+
+            },
+
+            {
+                name: "Google",
+                icon: "icon-google",
+                url: "https://www.google.com/"
+
+            }
+
+        ]
+    },
+    {
+        name: "个人网站",
+        icon: "icon-gerenwangzhan",
+        bigFly: [
+            {
+                name: "博客",
+                icon: "icon-boke",
+                url: "https://b.10999901.xyz/"
+            },{
+                name: "博客OA",
+                icon: "icon-oabangong-copy",
+                url: "https://b.10999901.xyz/admin"
+            }, {
+                name: "短链系统",
+                icon: "icon-shandian",
+                url: "https://sink.10999901.xyz/"
+            }, {
+                name: "云图库",
+                icon: "icon-tuku",
+                url: "https://sink.10999901.xyz/"
+            },{
+                name: "图库OA",
+                icon: "icon-oabangong-copy",
+                url: "https://sink.10999901.xyz/admin"
+            },
+
+        ]
+    }, {
+        name: "视频媒体",
+        icon: "icon-mv",
+        bigFly: [
+            {
+                name: "CCTV直播",
+                icon: "icon-cctv",
+                url: "https://tv.cctv.com/live/index.shtml"
+            },
+            {
+                name: "Youtube",
+                icon: "icon-youtube",
+                url: "https://www.youtube.com/"
+            }, {
+                name: "ACFUN",
+                icon: "icon-acfun",
+                url: "http://www.acfun.cn/index.html"
+            }, {
+                name: "哔哩哔哩",
+                icon: "icon-bilibili",
+                url: "https://www.bilibili.com/"
+            }, {
+                name: "虎牙直播",
+                icon: "icon-huyazhibo",
+                url: "https://www.huya.com/g/lol"
+            },
+        ]
+
+    }, {
+        name: "邮箱",
+        icon: "#icon-youxiang",
+        bigFly: [
+            {
+                name: "Gmail",
+                icon: "icon-gmail",
+                url: "https://mail.google.com/mail/u/0/#inbox"
+
+            },
+
+            {
+                name: "网易邮箱",
+                icon: "icon-wangyiyouxiang_24",
+                url: "https://mail.163.com/"
+
+            },{
+                name: "QQ邮箱",
+                icon: "icon-QQ",
+                url: "https://mail.qq.com/"
+
+            }
+
+        ]
+    },{
+        name: "工具",
+        icon: "icon-gongju",
+        bigFly: [
+            {
+                name: "Miku工具",
+                icon: "icon-gongju",
+                url: "https://tools.miku.ac/"
+
+            },{
+                name: "域名",
+                icon: "icon-godaddy",
+                url: "https://www.godaddy.com/"
+
+            },{
+                name: "Vercel",
+                icon: "icon-vercel",
+                url: "https://vercel.com/"
+
+            },
+          
+            {
+                name: "IP查询",
+                icon: "icon-ipchaxun",
+                url: "https://ip233.cn/"
+
+            },
+
+            {
+                name: "谷歌翻译",
+                icon: "icon-gugefanyi",
+                url: "https://translate.google.cn/?hl=zh-CN"
+
+            },
+
+            {
+                name: "Base64",
+                icon: "icon-base64",
+                url: "https://5iux.cn/base64/"
+
+            },
+
+            {
+                name: "站长Ping",
+                icon: "icon-ITDog",
+                url: "https://www.itdog.cn/ping"
+
+            },
+
+            {
+                name: "阿里矢量图标",
+                icon: "icon-keshihua-shiliang",
+                url: "https://www.iconfont.cn/"
+
+            },
+
+            {
+                name: "ChatGPT",
+                icon: "icon-ChatGPT",
+                url: "https://chatgpt.com"
+
+            }
+
+        ]
+    },
+    {
+        name: "开发",
+        icon: "icon-zhongguohangtiantubiaoheji-weizhuanlunkuo-",
+        bigFly: [
+            {
+                name: "W3school",
+                icon: "icon-w3school",
+                url: "http://www.w3school.com.cn/"
+
+            },
+
+            {
+                name: "Github",
+                icon: "icon-github",
+                url: "https://github.com/"
+
+            },
+            {
+                name: "Codepen",
+                icon: "icon-codepen",
+                url: "https://codepen.io/"
+
+            },
+            {
+                name: "吾爱破解",
+                icon: "icon-theater-masks",
+                url: "https://www.52pojie.cn/"
+
+            },
+            {
+                name: "SF思否",
+                icon: "icon-a-SegmentFaultsifou",
+                url: "https://segmentfault.com/"
+
+            },
+            {
+                name: "CdnJs",
+                icon: "icon-SCDN",
+                url: "https://cdnjs.com/"
+
+            },
+            {
+                name: "Font A.",
+                icon: "icon-font-awesome",
+                url: "https://fontawesome.com/icons?d=gallery&m=free"
+               
+            },
+            {
+                name: "MSDN下载",
+                icon: "icon-Windows",
+                url: "https://msdn.itellyou.cn/"
+
+            },
+            {
+                name: "C. flare",
+                icon: "icon-cloudflare-1",
+                url: "https://dash.cloudflare.com/"
+
+            },
+            {
+                name: "Swiper",
+                icon: "icon-suowo",
+                url: "https://www.swiper.com.cn/"
+
+            },
+            
+
+        ]
+    },
+
+
+];
---
+++ resources/js/dom.js
@@ -0,0 +1,35 @@
+$(function () {
+
+
+            // 清空原有内容
+            $('#jjjjjjjjjjjjjjj').empty();
+            for (let i = 0; i < bigg.length; i++) {
+                var bigKuang = `
+                                <ul class="mylist row" id="lllllllllll`+ i + `">
+                                <li class="title"  ><svg class="icon" aria-hidden="true">
+                                        <use xlink:href=#"`+ bigg[i].icon + `"></use>
+                                    </svg>`+ bigg[i].name + `</li>
+                                
+                                </ul>
+                            `;
+                $('#jjjjjjjjjjjjjjj').append(bigKuang);
+               
+                for (let j = 0; j < bigg[i].bigFly.length; j++) {
+
+                    var ssmKuang = `<li class="col-3 col-sm-3 col-md-3 col-lg-1"><a rel="nofollow" href="` + bigg[i].bigFly[j].url + `"
+                                        target="_blank"><svg class="icon" aria-hidden="true">
+                                            <use xlink:href="#`+ bigg[i].bigFly[j].icon + `"></use>
+                                        </svg><span>`+ bigg[i].bigFly[j].name + `</span></a></li>`;
+
+
+                   
+                    $('#lllllllllll' + i).append(ssmKuang);
+
+
+
+                }
+
+
+
+            };
+        });
